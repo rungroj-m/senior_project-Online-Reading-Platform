15c57556a86123ada5f0eedb740387570f4ace0a
Atit Leelasuksan <grief.d.lament@gmail.com>
2016-03-17 19:27:17 +0700

    subscription system - migration, controller, simple button and view

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests;
+use Auth;
+use DB;
+use App\Http\Controllers\Controller;
+
+class SubscriptionController extends Controller
+{
+    /**
+     * Display a listing of subscribed book.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user_id = Auth::id();
+        $subscribes = DB::table('subscriptions')->where('user_id', $user_id);
+        return view('profile.subscription')->with('subscribes', $subscribes);
+    }
+
+    /**
+     * subscribe a book.
+     *
+     * @param  int  $id of book to subscribe
+     * @return \Illuminate\Http\Response
+     */
+    public function subscribe($id)
+    {
+        $user_id = Auth::id();
+        DB::table('subscriptions')->insert(
+            ['user_id' => $user_id, 'book_id' => $id, 'active' => true]
+        );
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id of book to unsubscribe
+     * @return \Illuminate\Http\Response
+     */
+    public function unsubscribe($id)
+    {
+        $user_id = Auth::id();
+        DB::table('subscriptions')
+          ->where('user_id', $user_id)
+          ->where('book_id', $id)
+          ->update(['active' => false]);
+        return redirect()->back();
+    }
+}
