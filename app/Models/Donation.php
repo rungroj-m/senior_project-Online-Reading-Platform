562e55882ceda435099277051064a55004ab09b3
Atit Leelasuksan <grief.d.lament@gmail.com>
2016-04-08 13:43:33 +0700

    Migration and model for donation system

---
+++ app/Models/Donation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $fillable = array('user_id', 'book_id', 'goal_amount', 'active');
+    protected $primaryKey = 'id';
+
+    public function owner() {
+      return $this->belongsTo('App\Models\User');
+    }
+
+    public function book() {
+      return $this->belongsTo('App\Models\Book');
+    }
+}
