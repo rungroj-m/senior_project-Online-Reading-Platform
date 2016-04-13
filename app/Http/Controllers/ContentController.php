911a92a0eb1f814efeec47bac935bb999f4d5ed9
Rungroj Maipradit <Doom_iceza@hotmail.com>
2016-04-13 21:55:37 +0700

    change upload with docx

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -20,6 +20,7 @@ use Facebook\Facebook;
 use Auth;
 use Session;
 use Route;
+use \XMLReader ;
 
 class ContentController extends Controller {
 
@@ -106,7 +107,7 @@ class ContentController extends Controller {
 			// $content->content = str_replace("\r\n", "<br/>", $request->content);
 			$book->contents()->save($content);
 			// notify subscribed user
-//			$this->notify($book, $content);
+			$this->notify($book, $content);
 			return redirect($this->getURI($id).'/'.$id);
 		}
 	}
@@ -139,10 +140,6 @@ class ContentController extends Controller {
 	}
 
 	function read_file_docx($filename){
-
-		$striped_content = '';
-		$content = '';
-
 		if(!$filename || !file_exists($filename)) return false;
 
 		$zip = zip_open($filename);
@@ -155,22 +152,91 @@ class ContentController extends Controller {
 
 			if (zip_entry_name($zip_entry) != "word/document.xml") continue;
 
-			$content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+		$content = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
 
-			zip_entry_close($zip_entry);
+		zip_entry_close($zip_entry);
 		}// end while
 
 		zip_close($zip);
 
-		//echo $content;
-		//echo "<hr>";
-		//file_put_contents('1.xml', $content);
+		file_put_contents('temp.xml', $content);
 
-		$content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
-		$content = str_replace('</w:r></w:p>', "\r\n", $content);
-		$striped_content = strip_tags($content);
+		$xmlFile = "temp.xml";
+		// set location of docx text content file
+		$reader = new XMLReader;
+		$reader->open($xmlFile);
 
-		return $striped_content;
+		// set up variables for formatting
+		$text = ''; $formatting['bold'] = 'closed'; $formatting['italic'] = 'closed'; $formatting['underline'] = 'closed'; $formatting['header'] = 0;
+
+		// loop through docx xml dom
+		while ($reader->read()){
+			// look for new paragraphs
+			if ($reader->nodeType == XMLREADER::ELEMENT && $reader->name === 'w:p'){
+				// set up new instance of XMLReader for parsing paragraph independantly
+				$paragraph = new XMLReader;
+				$p = $reader->readOuterXML();
+				$paragraph->xml($p);
+
+				// search for heading
+				preg_match_all('/<w:pStyle w:val="(Heading.*?[1-6])"/',$p,$matches);
+				switch($matches[1]){
+					case 'Heading1': $formatting['header'] = 1; break;
+					case 'Heading2': $formatting['header'] = 2; break;
+					case 'Heading3': $formatting['header'] = 3; break;
+					case 'Heading4': $formatting['header'] = 4; break;
+					case 'Heading5': $formatting['header'] = 5; break;
+					case 'Heading6': $formatting['header'] = 6; break;
+					default:  $formatting['header'] = 0; break;
+				}
+
+				// open h-tag or paragraph
+				$text .= ($formatting['header'] > 0) ? '<h'.$formatting['header'].'>' : '<p>';
+
+				// loop through paragraph dom
+				while ($paragraph->read()){
+					// look for elements
+					if ($paragraph->nodeType == XMLREADER::ELEMENT && $paragraph->name === 'w:r'){
+						$node = trim($paragraph->readInnerXML());
+
+						// add <br> tags
+						if (strstr($node,'<w:br ')) $text .= '<br>';
+
+						// look for formatting tags
+						$formatting['bold'] = (strstr($node,'<w:b/>')) ? (($formatting['bold'] == 'closed') ? 'open' : $formatting['bold']) : (($formatting['bold'] == 'opened') ? 'close' : $formatting['bold']);
+						$formatting['italic'] = (strstr($node,'<w:i/>')) ? (($formatting['italic'] == 'closed') ? 'open' : $formatting['italic']) : (($formatting['italic'] == 'opened') ? 'close' : $formatting['italic']);
+						$formatting['underline'] = (strstr($node,'<w:u ')) ? (($formatting['underline'] == 'closed') ? 'open' : $formatting['underline']) : (($formatting['underline'] == 'opened') ? 'close' : $formatting['underline']);
+
+						// build text string of doc
+						$text .=     (($formatting['bold'] == 'open') ? '<strong>' : '').
+							(($formatting['italic'] == 'open') ? '<em>' : '').
+							(($formatting['underline'] == 'open') ? '<u>' : '').
+							htmlentities(iconv('UTF-8', 'ASCII//TRANSLIT',$paragraph->expand()->textContent)).
+							(($formatting['underline'] == 'close') ? '</u>' : '').
+							(($formatting['italic'] == 'close') ? '</em>' : '').
+							(($formatting['bold'] == 'close') ? '</strong>' : '');
+
+						// reset formatting variables
+						foreach ($formatting as $key=>$format){
+							if ($format == 'open') $formatting[$key] = 'opened';
+							if ($format == 'close') $formatting[$key] = 'closed';
+						}
+					}
+				}
+				$text .= ($formatting['header'] > 0) ? '</h'.$formatting['header'].'>' : '</p>';
+			}
+
+		}
+		$reader->close();
+
+		// suppress warnings. loadHTML does not require valid HTML but still warns against it...
+		// fix invalid html
+		$doc = new \DOMDocument();
+		$doc->encoding = 'UTF-8';
+		@$doc->loadHTML($text);
+		$goodHTML = simplexml_import_dom($doc)->asXML();
+		return $goodHTML;
+//		return 'a';
 	}
 
 
@@ -186,6 +252,8 @@ class ContentController extends Controller {
 		foreach($subs as $sub) {
 			if($sub->active) {
 				$user = $sub->user;
+				if($user->facebook_id)
+					$this->facebookNotification($user);
 				Notifynder::category('book.updatechapter')
 						->from('App\Models\Book', $book->id)
 						->to('App\Models\User', $user->id)
@@ -196,8 +264,7 @@ class ContentController extends Controller {
 		        $m->from('readi.notification@gmail.com', 'Readi');
 						$m->to($user->email, $user->username)->subject('Readi Notification');
 				});
-				if($user->facebook_id)
-					$this->facebookNotification($user);
+
 			}
 		}
 	}
