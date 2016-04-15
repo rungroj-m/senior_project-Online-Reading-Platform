3ebb8cad0b315cfc89dd307d6fb859431db57713
Atit Leelasuksan <grief.d.lament@gmail.com>
2016-04-16 01:50:05 +0700

    add missing script tag ">"

--- resources/views/chrome.blade.php
+++ resources/views/chrome.blade.php
@@ -14,10 +14,13 @@
 	<!-- Custom Fonts -->
  	<link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<!-- <link rel="stylesheet" href="asset('owlcarousel/owl.carousel.min.css')">
+	<link rel="stylesheet" href="asset('owlcarousel/owl.theme.default.min.css')"> -->
 </head>
 <footer>
 	<script src="{{ asset('js/app.js') }}"></script>
-	<script src="{{ asset('jquery/dist/jquery.min.js') }}"</script>
+	<script src="{{ asset('jquery/dist/jquery.min.js') }}"></script>
+	<!-- <script src="{{ asset('owlcarousel/owl.carousel.min.js') }}"></script> -->
 	<script src="{{ asset('js/templates.js') }}"></script>
 	<script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
 
@@ -27,7 +30,7 @@
 			plugins: 'textcolor link image',
 			toolbar: 'undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
 			menubar: false,
-			forced_root_block : "", 
+			forced_root_block : "",
 			force_br_newlines : true,
 			force_p_newlines : false,
 			content_css: "css/app_custom.css",
