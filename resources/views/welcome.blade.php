e1e4e2fac37714f8738aedbf6e40fe06bbe6b085
Pawin <h2so4noel@gmail.com>
2016-04-16 17:13:43 +0700

    Update index page with query sort.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,30 +2,29 @@
 
 @section('content')
 
-<body class="home-background">
+<body class="main-container home-background">
 	<div class="row">
-		<div class="container content">
-			<div class="col-md-5 col-md-offset-1 home-left">
+		<div class="container col-md-10">
+			<div class="col-md-6 home-left">
 				<div class="title">
 					<span class="first-letter">R</span>EADI
 				</div>
-				<h2>I don't have a slogan yet, so anything would do here.</h2>
+				<h2>Reading Scrublords PJSalt Kappa FailFish.</h2>
 			</div>
-			<div class="col-md-5 home-right">
+			<div class="col-md-6 home-right">
 				<form class="form-vertical" role="form" method="POST" action="{{ url('/login') }}">
 					<h1><span class="first-letter">W</span>elcome!</h1>
 					<p>Login to start reading, become a creator, or <a href="register">register</a> to be part of our community</p><br/>
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 					<div class="form-group">
-						<div class="col-md-10 col-md-offset-1">
+						<div>
 							<input placeholder="E-mail" type="email" class="form-control" name="email" value="{{ old('email') }}">
 							<input placeholder="Password" type="password" class="form-control" name="password"><br/>
 							<button type="submit" class="form-control btn btn-success">Login</button>
-						</div>
+						</div><br/>
 					</div>
 				</form>
 			</div>
-			<div class="col-md-1"></div>
 		</div>
 	</div>
 </body>
