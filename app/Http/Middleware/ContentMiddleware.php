2580298ddb5bdb685d451f40f0fd3650d8bc4358
Rungroj Maipradit <Doom_iceza@hotmail.com>
2016-04-19 14:22:43 +0700

    fix content middle

--- app/Http/Middleware/ContentMiddleware.php
+++ app/Http/Middleware/ContentMiddleware.php
@@ -6,6 +6,7 @@ use Closure;
 use DB;
 use App\Models\Book;
 use Illuminate\Support\Facades\Auth;
+use PhpSpec\Exception\Exception;
 
 class ContentMiddleware
 {
@@ -21,12 +22,25 @@ class ContentMiddleware
         $id = $request->book;
         $chapter = $request->chapter;
         $book = Book::find($id);
-        $content_chap = DB::table('books_contents')
-            ->where('book_id', $id)
-            ->join('contents', 'books_contents.content_id', '=', 'contents.id')
-            ->where('contents.chapter', $chapter)->first();
-        if($content_chap->private && !$book->isOwner() && !Auth::user()->isAdmin())
-            return redirect('books/'.$id)->with('status', 'You are not allowed.');
-        return $next($request);
+        try {
+            $content_chap = DB::table('books_contents')
+                ->where('book_id', $id)
+                ->join('contents', 'books_contents.content_id', '=', 'contents.id')
+                ->where('contents.chapter', $chapter)->first();
+            if($content_chap->private && !$book->isOwner() && !Auth::user()->isAdmin())
+                return redirect($this->getURI($id).'/'.$id)->with('status', 'You are not allowed.');
+            return $next($request);
+        }catch (\ErrorException $e){
+            return redirect($this->getURI($id).'/'.$id);
+        }
+    }
+
+
+    private function getURI($id){
+        $book = Book::findOrfail($id);
+        if($book->isComic())
+            return 'comics';
+        else
+            return 'books';
     }
 }
