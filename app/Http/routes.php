5bb912377a7ada63176ee0a3c621de52bae467cb
Rungroj Maipradit <Doom_iceza@hotmail.com>
2016-04-20 12:24:23 +0700

    add route post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,6 +9,8 @@ Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'Auth\AuthController@getLogout');
 
+Route::post('index', 'WelcomeController@index');
+
 //Social Login
 Route::get('/login/{provider?}',[
 	'uses' => 'Auth\AuthController@getSocialAuth',
